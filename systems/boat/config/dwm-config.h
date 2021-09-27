4d40f86d8544645bb86bcc407d5412a117af618b
Alexander Nortung <alex_nortung@live.dk>
2021-09-27 10:16:29 +0200

    added systems/boat

---
+++ systems/boat/config/dwm-config.h
@@ -0,0 +1,180 @@
+/* See LICENSE file for copyright and license details. */
+
+/* 
+    You obviously need the X11 development package installed, but here is the upstream copy
+    of this header if you can't bother using the contents of your own hard drive. ;-P
+    https://cgit.freedesktop.org/xorg/proto/x11proto/tree/XF86keysym.h
+*/
+
+#include <X11/XF86keysym.h>
+
+// Volume buttons
+static const char *upvol[]   = { "amixer", "set", "Master", "3%+",     NULL };
+static const char *downvol[] = { "amixer", "set", "Master", "3%-",     NULL };
+static const char *unmutevol[] = { "amixer", "set", "Master", "unmute",     NULL };
+static const char *mutevol[] = { "amixer", "set", "Master", "toggle", NULL };
+
+static const char *mic_mute_toggle[] = { "amixer", "set", "Capture", "toggle", NULL };
+
+// Media buttons
+static const char *media_playpause_cmd[] = { "playerctl", "play-pause", NULL };
+static const char *media_next_cmd[] = { "playerctl", "next", NULL};
+static const char *media_previous_cmd[] = { "playerctl", "previous", NULL};
+static const char *media_stop_cmd[] = { "playerctl", "stop", NULL};
+static const char *monitor_brightness_up_cmd[] = { "brightnessctl", "set", "+5%" };
+static const char *monitor_brightness_down_cmd[] = { "brightnessctl", "set", "5%-" };
+
+// Start programs
+static const char *open_firefox_cmd[] = { "firefox", NULL };
+static const char *open_flameshot_cmd[] = { "flameshot", "gui", NULL };
+
+// Lock, shutdown, suspend, hibernate
+static const char *suspend_cmd[] =  { "systemctl", "suspend", NULL };
+static const char *poweroff_cmd[] =  { "poweroff", NULL };
+static const char *reboot_cmd[] =  { "reboot", NULL };
+
+// Systray
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Hasklig:size=12", "TerminessTTF Nerd Font:size=12" };
+static const char dmenufont[]       = "Hasklig:size=12";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+// BAR
+//static const int showbar       = 1;          /* 0 means no bar */
+//static const int topbar        = 1;          /* 0 means bottom bar */
+//static const int usealtbar     = 1;          /* 1 means use non-dwm status bar */
+//static const char *altbarclass = "Polybar";  /* Alternate bar class name */
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+};
+
+/* key definitions */
+//#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
+
+static Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_End,      quit,           {0} }, // do not quit
+    /* With 0 as modifier, you are able to use the keys directly */
+    { 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+    { 0,                       XF86XK_AudioLowerVolume, spawn, {.v = unmutevol } },
+    { 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
+    { 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+    { 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = unmutevol   } },
+    { 0,                       XF86XK_AudioMicMute, spawn, {.v = mic_mute_toggle   } },
+    // Media keys
+    { 0, XF86XK_AudioPlay, spawn, {.v = media_playpause_cmd } },
+    { 0, XF86XK_AudioNext, spawn, {.v = media_next_cmd } },
+    { 0, XF86XK_AudioPrev, spawn, {.v = media_previous_cmd } },
+    { 0, XF86XK_AudioStop, spawn, {.v = media_stop_cmd } },
+    { 0, XF86XK_MonBrightnessUp, spawn, {.v = monitor_brightness_up_cmd } },
+    { 0, XF86XK_MonBrightnessDown, spawn, {.v = monitor_brightness_down_cmd } },
+    // Open programs
+    { MODKEY, XK_F2, spawn, {.v = open_firefox_cmd} },
+    { 0, XK_Print, spawn, {.v = open_flameshot_cmd} },
+    // Power
+    { MODKEY, XK_F9, spawn, {.v = suspend_cmd } },
+    { MODKEY|ShiftMask, XK_F12, spawn, {.v = poweroff_cmd } },
+    { MODKEY|ShiftMask, XK_F11, spawn, {.v = reboot_cmd } },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
---
+++ systems/boat/configuration.nix
@@ -0,0 +1,285 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+let
+  unstable = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs/";
+    rev = "b01baaebac95d24a51be0ededd92e3a965569605";
+  }) { 
+    config = config.nixpkgs.config;
+  };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ../../common/nvim.nix
+      ../../common/programming-pkgs.nix
+      ../../common/comfort-packages.nix
+      ../../common/sound.nix
+      ../../common/console.nix
+    ];
+
+  nixpkgs.config = {
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+      "discord"
+      "spotify" "spotify-unwrapped"
+    ];
+  };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      st = super.st.overrideAttrs (oldAttrs : rec {
+        buildInputs = oldAttrs.buildInputs ++ [ pkgs.harfbuzz ];
+        configFile = super.writeText "config.h" (builtins.readFile ./config/st-config.h);
+        postPatch = "${oldAttrs.postPatch}\ncp ${configFile} config.def.h\n";
+        patches = [
+          # Scrollback
+          (super.fetchpatch {
+            url = "https://st.suckless.org/patches/scrollback/st-scrollback-0.8.4.diff";
+            sha256 = "0valvkbsf2qbai8551id6jc0szn61303f3l6r8wfjmjnn4054r3c";
+          })
+          # Alpha
+          (super.fetchpatch {
+            url = "https://st.suckless.org/patches/alpha/st-alpha-0.8.2.diff";
+            sha256 = "158k93bbgrmcajfxvkrzfl65lmqgj6rk6kn8yl6nwk183hhf5qd4";
+          })
+          # Ligatures
+          (super.fetchpatch {
+            url = "https://st.suckless.org/patches/ligatures/0.8.3/st-ligatures-alpha-scrollback-20200430-0.8.3.diff";
+            sha256 = "1y6fl31fz1ks43v80ccisz781zzf6fgaijdhcbvkxy2d009xla27";
+          })
+          # Desktop entry - gives an icon
+          (super.fetchpatch {
+            url = "https://st.suckless.org/patches/desktopentry/st-desktopentry-0.8.4.diff";
+            sha256 = "0v0hymybm2yplrvdjqysvcyhl36a5llih8ya9xjim1fpl609hg8y";
+          })
+          # More icon
+          (super.fetchpatch {
+            url = "http://st.suckless.org/patches/netwmicon/st-netwmicon-0.8.4.diff";
+            sha256 = "0gnk4fibqyby6b0fdx86zfwdiwjai86hh8sk9y02z610iimjaj1n";
+          })
+
+        ];
+      });
+      dwm = super.dwm.overrideAttrs (oldAttrs : rec {
+        configFile = super.writeText "config.h" (builtins.readFile ./config/dwm-config.h);
+        postPatch = oldAttrs.postPatch or "" + "\necho 'Using own config file...'\n cp ${configFile} config.def.h";
+        patches = [
+          # alwaysfullscreen - focus does not drift off when in fullscreen
+          #(super.fetchpatch {
+          #  url = "https://dwm.suckless.org/patches/alwaysfullscreen/dwm-alwaysfullscreen-6.1.diff";
+          #  sha256 = "1037m24c1hd6c77p84szc5qqaw4kldwwfzggyn6ac5rv8l47j057";
+          #})
+          # Systray
+          (super.fetchpatch {
+            url = "https://dwm.suckless.org/patches/systray/dwm-systray-6.2.diff";
+            sha256 = "1p1hzkm5fa9b51v19w4fqmrphk0nr0wnkih5vsji8r38nmxd4ydp";
+          })
+        ];
+      });
+      dmenu = super.dmenu.overrideAttrs (oldAttrs : rec {
+        #postPatch = "${oldAttrs.postPatch}\ncp ${configFile} config.def.h\n";
+        patches = [
+          (super.fetchpatch {
+            url = "https://tools.suckless.org/dmenu/patches/case-insensitive/dmenu-caseinsensitive-5.0.diff";
+            sha256 = "0bb0iwv8d53dibzqc307y3z852whwxjzjrxkbs07cs5y3c2l98ay";
+          })
+        ];
+      });
+    })
+  ];
+
+  fonts.fonts = with pkgs; [
+    hasklig
+    terminus-nerdfont
+  ];
+
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    grub = {
+      version = 2;
+      devices = [ "nodev" ];
+      enable = true;
+      efiSupport = true;
+    };
+  };
+
+
+  # Set your time zone.
+  time.timeZone = "Europe/Copenhagen";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking = {
+    hostName = "boat";
+    useDHCP = false;
+    interfaces.enp0s31f6.useDHCP = true;
+    interfaces.wlp3s0.useDHCP = true;
+    networkmanager = {
+      enable = true;
+    };
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  services.dwm-status = {
+    enable = true;
+    order = [
+      "audio"
+      #"backlight"
+      "battery"
+      "cpu_load"
+      "network"
+      "time"
+    ];
+    extraConfig = ''
+      separator = " | "
+ 
+      [audio]
+      icons = [ "奄", "奔", "墳" ]
+      mute = "ﱝ"
+      template = "{ICO} {VOL}%"
+
+      [battery]
+      charging = ""
+      discharging = ""
+      no_battery = " No battery"
+      icons = ["", "", "", "", "", "", "", "", "", "", ""]
+
+      [cpu_load]
+      template = " {CL1}"
+      update_interval = 15
+
+      [network]
+      no_value = "睊 Not connected"
+      template = "直 {IPv4}"
+    '';
+  };
+
+  services.xserver = {
+    enable = true;
+    windowManager.dwm.enable = true;
+    displayManager.lightdm.enable = true;
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput = {
+      enable = true;
+      touchpad.tapping = true;
+      touchpad.naturalScrolling = true;
+    };
+    # Configure keymap in X11
+    layout = "dk";
+
+    xautolock = {
+      enable = true;
+      time = 10;
+      #locker = "${pkgs.slock}/bin/slock";
+      extraOptions = [
+        #"-lockaftersleep"
+        "-detectsleep"
+      ];
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "simple";
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users = {
+    users = {
+      alexander = {
+        shell = pkgs.zsh;
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "vboxusers"
+          "audio"
+        ];
+      };
+    };
+    extraGroups.vboxusers.members = [ "alexander" ];
+  };
+
+
+  environment.systemPackages = with pkgs; [
+    python39Packages.pygments
+    xss-lock
+    xorg.xev
+    xclip
+    i3lock
+    #i3lock-blur
+    brightnessctl # Brightness from terminal
+    dmenu
+    st
+    spotify
+    libreoffice
+    tmate
+    bvi # hex editor with vim bindings
+    unstable.session-desktop-appimage
+    texlive.combined.scheme-full
+    unstable.discord
+    zip unzip
+    flameshot
+    joplin-desktop
+    vim
+    firefox
+    zathura
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  programs.slock = {
+    enable = true;
+  };
+
+  programs.nm-applet = {
+    enable = true;
+  };
+
+  programs.xss-lock = {
+    enable = true;
+    lockerCommand = "${pkgs.xautolock}/bin/xautolock -locknow";
+  };
+
+  services.redshift = {
+    enable = true;
+    temperature.night = 2900;
+  };
+
+  virtualisation.virtualbox.host.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
+
