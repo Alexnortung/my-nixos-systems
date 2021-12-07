891b3df8e6629cac6a334fbcf9e0f2d08b08edd7
Alexander Nortung <alex_nortung@live.dk>
2021-12-07 11:38:38 +0100

    boat: dwm is now also nord theme

--- systems/boat/config/dwm-config.h
+++ systems/boat/config/dwm-config.h
@@ -45,11 +45,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hasklig:size=12", "TerminessTTF Nerd Font:size=12" };
 static const char dmenufont[]       = "Hasklig:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#2e3440";
+//static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#3b4252";
+//static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#e5e9f0";
+//static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#eceff4";
+//static const char col_cyan[]        = "#005577"; // Focused color
+static const char col_cyan[]        = "#3b4252"; // Focused color
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
