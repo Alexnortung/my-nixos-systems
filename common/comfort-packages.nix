97571ba0ef7ff1f5885a1ec1e92b2f50f101dd60
Alexander Nortung <alex_nortung@live.dk>
2021-12-30 17:49:32 +0100

    moved git-config

--- common/comfort-packages.nix
+++ common/comfort-packages.nix
@@ -1,43 +1,14 @@
 { pkgs, config, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    config = {
-      user = {
-        name = "Alexander Nortung";
-        email = "alex_nortung@live.dk";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      pull = {
-        rebase = false;
-      };
-      alias = {
-        st = "status";
-        co = "checkout";
-        lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
-        lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-        hale = "pull";
-        puff = "push";
-        hent = "fetch";
-        skyd = "branch";
-        forgren = "branch";
-        fastlag = "commit";
-        flet = "merge";
-        gem = "stash";
-        klandre = "blame";
-        marker = "tag";
-        klon = "clone";
-        tilfoj = "add";
-      };
-    };
-  };
+  imports = [
+    ./git-config.nix
+  ];
   programs.htop = {
     enable = true;
   };
   environment.systemPackages = with pkgs; [
+    git
     bash
     lm_sensors
     gnumake
