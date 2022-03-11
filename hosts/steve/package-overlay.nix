994f36a2f23bb9746b582ce890792bd113bcc287
Alexander Nortung <alex_nortung@live.dk>
2022-03-11 17:10:29 +0100

    steve: updated.

--- hosts/steve/package-overlay.nix
+++ hosts/steve/package-overlay.nix
@@ -1,5 +1,6 @@
 inputs@{ nur-alexnortung-steve, ... }:
 channels: ((import ../../overlays/pre-master.nix).overlays channels) ++ [
+  (import ../../overlays/shared-unstable.nix channels.nixpkgs)
   (import ../../overlays/default-unstable.nix channels.nixpkgs-unstable-steve)
   nur-alexnortung-steve.overlay
   (final: prev: {
