092c01f2dd5c5fcecadaddb596ee5907050c2989
Alexander Nortung <alex_nortung@live.dk>
2022-04-06 22:48:08 +0200

    removed old systems folder

--- systems/default.nix
+++
@@ -1,8 +0,0 @@
-{ self, ... }@inputs:
-
-{
-  boat = self.lib.mkSystem "boat" inputs.nixpkgs-boat "x86_64-linux";
-  #enderman = self.lib.mkSystem "enderman" inputs.nixpkgs-enderman "x86_64-linux";
-  #spider = self.lib.mkSystem "spider" inputs.nixpkgs-spider "x86_64-linux";
-  #steve = self.lib.mkSystem "steve" inputs.nixpkgs-steve "x86_64-linux";
-}
