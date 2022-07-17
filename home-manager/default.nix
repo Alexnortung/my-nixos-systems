1baea026faf3c2b5fbb16f716fad93a0cae96645
Alexander Nortung <alex_nortung@live.dk>
2022-07-17 14:46:49 +0200

    alexander@boat: added

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -2,7 +2,7 @@
 
 {
   configs = {
-    # "alexander@boat" = import ./configs/alexander_at_boat inputs;
+    "alexander@boat" = import ./configs/alexander_at_boat args;
     "alexander@steve" = import ./configs/alexander_at_steve args;
     "alexander@spider" = import ./configs/alexander_at_spider args;
   };
