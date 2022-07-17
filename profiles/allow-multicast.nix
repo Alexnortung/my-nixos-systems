7086943b2e6d2e66590f404468b2fdbd2ea73ad1
Alexander Nortung <alex_nortung@live.dk>
2022-07-17 13:03:00 +0200

    somehow multicast works now

--- profiles/allow-multicast.nix
+++ profiles/allow-multicast.nix
@@ -2,5 +2,15 @@
 {
   networking.firewall.extraCommands = ''
     iptables -I INPUT -m pkttype --pkt-type multicast -j ACCEPT
+    iptables -A INPUT -m pkttype --pkt-type multicast -j ACCEPT
+    iptables -I INPUT -p udp -m udp --match multiport --dports 1900,5353 -j ACCEPT
   '';
+  networking.firewall.allowedTCPPorts = [
+    8008 8009 8010
+  ];
+  networking.firewall.allowedUDPPorts = [
+    1900 # should be outbound
+    5353
+  ];
+    #iptables -I INPUT -m pkttype --pkt-type multicast -j ACCEPT
 }
