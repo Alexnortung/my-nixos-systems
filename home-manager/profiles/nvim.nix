38bbb38e9b0c376ac757ec2d778886485dfdd2c2
Alexander Nortung <alex_nortung@live.dk>
2022-07-20 00:46:46 +0200

    nvim: added cmp config

--- home-manager/profiles/nvim.nix
+++ home-manager/profiles/nvim.nix
@@ -74,6 +74,34 @@
       mapleader = " ";
 
       rainbow_active = 1;
+
+      kind_icons = {
+        Text = "";
+        Method = "m";
+        Function = "";
+        Constructor = "";
+        Field = "";
+        Variable = "";
+        Class = "";
+        Interface = "";
+        Module = "";
+        Property = "";
+        Unit = "";
+        Value = "";
+        Enum = "";
+        Keyword = "";
+        Snippet = "";
+        Color = "";
+        File = "";
+        Reference = "";
+        Folder = "";
+        EnumMember = "";
+        Constant = "";
+        Struct = "";
+        Event = "";
+        Operator = "";
+        TypeParameter = "";
+      };
     };
 
     plugins = {
@@ -166,18 +194,60 @@
               end
             '';
           };
+          "<S-Tab>" = {
+            modes = [ "i" "s" ];
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end
+            '';
+          };
         };
+
+        formatting = {
+          fields = [
+            "kind"
+            "abbr"
+            "menu"
+          ];
+          format = ''
+            function(entry, vim_item)
+              -- Kind icons
+              vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+              -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+              vim_item.menu = ({
+                luasnip = "[Snippet]",
+                buffer = "[Buffer]",
+                path = "[Path]",
+              })[entry.source.name]
+              return vim_item
+          end
+          '';
+        };
+
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; } #-- For luasnip users.
+          { name = "path"; }
+          { name = "buffer"; }
+        ];
+
+        # confirm_opts = {
+        #   behavior = "Replace";
+        #   select = false;
+        # };
+
+        experimental.ghost_text = true;
       };
     };
 
     extraPlugins = with pkgs.vimPlugins; [
-      # tcomment_vim
-      nvim-cmp
-      cmp-path
-      cmp-buffer
-      cmp-cmdline
-      cmp-nvim-lsp
-      cmp_luasnip
       luasnip
       friendly-snippets
       vim-sleuth # detects indentation
