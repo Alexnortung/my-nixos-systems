fdc9e582f5f200a5b73757eeb02aa4c62b93e426
Alexander Nortung <alex_nortung@live.dk>
2022-12-25 15:08:54 +0100

    remvoed old vpn

--- modules/personal-vpn.nix
+++ modules/personal-vpn.nix
@@ -23,19 +23,5 @@
         }
       ];
     };
-    wg0 = {
-      # set address on each system.
-      # set privateKeyFile on each system.
-      listenPort = 51821;
-      peers = [
-        {
-          publicKey = "9WrHJEt/yzULE8IOLV0JkkA/8ult0RYg+buVuC7dfFU=";
-          allowedIPs = [ "10.100.0.0/16" ]; # send all comunication to 10.100.x.x through wg0
-          endpoint = "142.93.130.164:51820";
-          # endpoint = "86.48.0.131:51820";
-          persistentKeepalive = 25; # Keep nat tables alive
-        }
-      ];
-    };
   };
 }
