01cee9557253f11c213e5c9985d0b49d4b47331c
Alexander Nortung <alex_nortung@live.dk>
2023-01-05 22:31:01 +0100

    changed dns settings

---
+++ hosts/enderman/dns.nix
@@ -0,0 +1,23 @@
+{
+  networking = {
+    # my dns
+    hosts = {
+      "192.168.3.20" = [
+        "jellyfin.northwing.games"
+      ];
+    };
+    # ad blocking
+    stevenBlackHosts.enable = true;
+  };
+
+  services.dnsmasq = {
+    enable = true;
+    extraConfig = ''
+      port=53
+      # Never forward plain names (without a dot or domain part)
+      domain-needed
+      # Never forward addresses in the non-routed address spaces.
+      bogus-priv
+    '';
+  };
+}
