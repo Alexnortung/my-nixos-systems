f610e92cb9e271fa5941a922a50ac3d1e68495e7
Alexander Nortung <alex_nortung@live.dk>
2023-06-18 15:52:54 +0200

    enderman: moved to ssd

--- hosts/enderman/hardware-configuration.nix
+++ hosts/enderman/hardware-configuration.nix
@@ -8,29 +8,36 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/561a42ce-2314-4c71-9a3b-795783863152";
+    { device = "/dev/disk/by-uuid/dc199d98-e0ed-4616-99dd-fdb689d54c52";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8B03-EC38";
+    { device = "/dev/disk/by-uuid/73A2-3F6E";
       fsType = "vfat";
     };
 
-  fileSystems."/data/data1" =
-    { device = "/dev/disk/by-uuid/822601c9-fcec-444b-9703-e48f323ded12";
-      fsType = "ext4";
-    };
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/1f2cdde6-626f-4fd3-acf5-908ab0839051"; }
+    ];
 
-  swapDevices = [ ];
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
 }
-
