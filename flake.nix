d4038d93bdc3e5610b99b37f07886758f6415adf
Alexander Nortung <alex_nortung@live.dk>
2024-11-16 11:06:10 +0100

    update factorio

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       url = "github:alexnortung/nixpkgs/s3fs-module";
     };
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-unstable-master.url = "github:greaka/nixpkgs/factorio";
+    nixpkgs-unstable-master.url = "github:Infinidoge/nixpkgs/bump/factorio";
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
     };
