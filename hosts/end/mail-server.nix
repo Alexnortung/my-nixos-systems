2ecab2d8cef4dd42449917fc8a72ec371a9b5bde
Alexander Nortung <alex_nortung@live.dk>
2025-04-20 22:16:58 +0200

    end: Added roundcube

--- hosts/end/mail-server.nix
+++ hosts/end/mail-server.nix
@@ -2,13 +2,17 @@
   inputs,
   config,
   ...
-}: {
+}:
+{
   mailserver = {
     enable = true;
     openFirewall = true;
     certificateScheme = "acme-nginx";
     fqdn = "mails.northwing.games";
-    domains = [ "northwing.games" "nortung.dk" ];
+    domains = [
+      "northwing.games"
+      "nortung.dk"
+    ];
 
     # A list of all login accounts. To create the password hashes, use
     # nix run nixpkgs#apacheHttpd -c htpasswd -nbB "" "super secret password" | cut -d: -f2
@@ -21,11 +25,24 @@
       };
       "an@northwing.games" = {
         hashedPasswordFile = config.age.secrets.mail-pass-admin.path;
-        aliases = ["alexander@northwing.games"];
+        aliases = [ "alexander@northwing.games" ];
       };
       "alexander@nortung.dk" = {
         hashedPasswordFile = config.age.secrets.mail-pass-admin.path;
       };
     };
+
+  };
+
+  services.roundcube = {
+    enable = true;
+    hostName = "mails.northwing.games";
+    extraConfig = ''
+      # starttls needed for authentication, so the fqdn required to match
+      # the certificate
+      $config['smtp_server'] = "tls://${config.mailserver.fqdn}";
+      $config['smtp_user'] = "%u";
+      $config['smtp_pass'] = "%p";
+    '';
   };
 }
